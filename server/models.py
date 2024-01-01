e6179a0ce27f8edf391eddac8f3f56beb65ca900
morrison <morrisonwanja@gmail.com>
2024-01-02 00:19:53 +0300

    Complete Assignment

--- server/models.py
+++ server/models.py
@@ -7,3 +7,12 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    edited_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+    def __repr__(self):
+        return f'<Name {self.name}, image {self.image} price {self.price}.>'
